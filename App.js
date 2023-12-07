7811393b0abfa4a58764a2391b352a3d1fd6e6a7
Thirivikraman K M <salem.vikram@gmail.com>
2023-12-07 17:57:43 +0530

    Igniting our app

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
 const heading = React.createElement(
     "div",
     {id:"Parent"},
     [
-        React.createElement("div",{id:"child1"},[React.createElement("h1",{},"this is H1"),
-        React.createElement("h2",{},"this is H2")]),
+        React.createElement("div",{id:"child1"},[React.createElement("h1",{},"tis is H1"),
+        React.createElement("h2",{},"this is Vikram")]),
         React.createElement("div",{id:"child2"},[React.createElement("h1",{},"this is H1"),
         React.createElement("h2",{},"this is H2")])
     ]
